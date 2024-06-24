cae84aac89e1e646b800a0c764f6362e2d78b1e3
Gordey Guselnikov <g.guselnikov@bi.zone>
2024-06-25 01:10:10 +0700

    Настройка проекта, удаление сторибордов, Firebase pod init and install, AuthManager

---
+++ PhoneAuth/AccountViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AccountViewController.swift
+//  PhoneAuth
+//
+//  Created by Guselnikov Gordey on 24.06.24.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+    }
+
+}
