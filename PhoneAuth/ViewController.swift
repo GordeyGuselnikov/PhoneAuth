91224b6a73fd773c23fe93dbebd660de656fff95
Gordey Guselnikov <g.guselnikov@bi.zone>
2024-06-24 22:45:23 +0700

    Initial Commit

---
+++ PhoneAuth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhoneAuth
+//
+//  Created by Guselnikov Gordey on 24.06.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
